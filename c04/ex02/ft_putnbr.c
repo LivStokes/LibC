66ebb3f8d1403d9f912490e28e29dc0ebc7067f9
LivStokes <Oliviastokess02@gmail.com>
2025-04-14 16:24:57 +0200

    Add files via upload

--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: olstokes <olstokes@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 17:07:33 by olstokes          #+#    #+#             */
-/*   Updated: 2025/04/13 18:34:56 by olstokes         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:21:17 by olstokes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,28 +19,30 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	long int	nbr;
-
-	nbr = nb;
-	if (nbr < 0)
-	{
-		ft_putchar('-');
-		nbr = -nbr;
-	}
-	if (nbr > 9)
-	{
-		ft_putnbr(nbr / 10);
-		ft_putnbr(nbr % 10);
-	}
+	if (nb == -2147483648)
+		write(1, "-2147483648", 11);
 	else
 	{
-		ft_putchar('0' + nbr);
+		if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+		}
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+			ft_putchar(nb % 10 + '0');
+		}
+		else
+		{
+			ft_putchar(nb + '0');
+		}
 	}
 }
 
 int	main(void)
 {
-	ft_putnbr(-2147483648);
+	ft_putnbr(2147483647);
 	ft_putchar('\n');
+	return (0);
 }
-
